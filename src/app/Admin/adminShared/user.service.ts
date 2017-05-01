9d455df46eb90bb1f46a822a2cb40887ca228576
RonAlmog <ron8132@gmail.com>
2017-05-01 00:05:29 -0400

    signup page with firebase

--- src/app/Admin/adminShared/user.service.ts
+++ src/app/Admin/adminShared/user.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import * as firebase from 'firebase';
 
 @Injectable()
 export class UserService implements CanActivate{
     userLoggedIn: boolean = false;
+    loggedInUser: string;
+    authUser: any;
 
-    constructor(private router: Router){}
+    constructor(private router: Router){
+        firebase.initializeApp({
+           
+        })
+    }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean{
         let url: string = state.url;
@@ -18,6 +25,43 @@ export class UserService implements CanActivate{
 
         this.router.navigate(['/admin/login']);
         return false;
+    }
+
+    register(email: string, password: string){
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .catch(function(error){
+                alert(`${error.message} Please try again!`);
+
+            });
+    }
+
+    verifyUser(){
+        this.authUser = firebase.auth().currentUser;
+
+        if(this.authUser){
+            alert(`Welcome ${this.authUser.email}`);
+            this.loggedInUser = this.authUser.email;
+            this.userLoggedIn = true;
+            this.router.navigate(['/admin']);
+
+        }
+    }
+
+    login(loginEmail: string, loginPassword: string){
+        firebase.auth().signInWithEmailAndPassword(loginEmail, loginPassword)
+            .catch(function(error){
+                alert(`${error.message} Unable to login. Try again!`);
+            });
+    }
+
+    logout(){
+        this.userLoggedIn = false;
+        firebase.auth().signOut().then(function(){
+            alert(`Logged Out!`);
+        }, function(error){
+            alert(`${error.message} Unable to logout. Try again!`);
+        });
+
     }
 
 }
