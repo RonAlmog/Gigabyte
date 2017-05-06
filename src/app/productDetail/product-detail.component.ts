0bdc4e29bfcfb2614b020ba7a3d1997bac34fdfc
RonAlmog <ron8132@gmail.com>
2017-05-06 14:21:18 -0400

    product detail page and comonent

---
+++ src/app/productDetail/product-detail.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params} from '@angular/router';
+import * as firebase from 'firebase';
+import { Product } from '../admin/adminShared/product';
+
+@Component({
+    templateUrl: './product-detail.component.html',
+    styleUrls: ['./product-detail.component.css']
+})
+
+export class ProductDetailComponent implements OnInit{
+    prod: Product;
+
+    constructor(private route: ActivatedRoute, private router: Router){}
+
+    ngOnInit(){
+        let prodId = this.route.snapshot.params['id'];
+        this.getSingle(prodId);
+    }
+
+    getSingle(id: string){
+        let dbRef = firebase.database().ref('products');
+        dbRef.orderByChild('id')
+            .equalTo(id)
+            .once('value')
+            .then((snapshot)=>{
+                let tmp = snapshot.val();
+                let transform = Object.keys(tmp).map(key=>tmp[key]);
+                let name = transform[0].name;
+                let description =  transform[0].description;
+                let imgTitle =  transform[0].imgTitle;
+                let img =  transform[0].img;
+                let price = transform[0].price;
+                let id = transform[0].id;
+                this.prod = new Product(name, description, imgTitle, img, price, id);
+            });
+    }
+
+}
+
